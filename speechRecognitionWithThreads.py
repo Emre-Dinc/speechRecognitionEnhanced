3e083c85a2ddfc6e21a270e51ae9211cdd16d3cf
inancsege <serhatege2000@gmail.com>
2022-12-17 00:12:46 +0300

    Added audio file seperation and merging functions

--- speechRecognitionWithThreads.py
+++ speechRecognitionWithThreads.py
@@ -1,22 +1,18 @@
+import csv
+import os
+import wave
 from threading import Thread
 
 import noisereduce as nr
 import soundfile as sf
 import speech_recognition as sr
 from pyannote.audio import Pipeline
-import numpy as np  # linear algebra
-import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
-from pyannote.database import get_protocol, FileFinder
-from pyannote.audio.pipelines import VoiceActivityDetection
-import matplotlib.pyplot as plt
-import numpy as np
-import csv
+from pydub import AudioSegment
 
 try:
     from queue import Queue  # Python 3 import
 except ImportError:
     from Queue import Queue  # Python 2 import
-
 
 with open('SignList_ClassId_TR_EN.csv') as f:
     turkish = [row[1] for row in csv.reader(f)]
@@ -30,17 +26,18 @@ print(english)
 pipeline = Pipeline.from_pretrained("pyannote/speaker-segmentation",
                                     use_auth_token="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")
 
-r = sr.Recognizer()
+record = sr.Recognizer()
+record_for_another_recognizer = sr.Recognizer()
 audio_queue = Queue()
-sampleRate = 22500
+sampleRate = 32000
 
-print("minimum enerji eşiği belirleniyor {}".format(r.energy_threshold))
+print("minimum enerji eşiği belirleniyor {}".format(record.energy_threshold))
 
 
 def recognize_worker():
     # this runs in a background thread
     while True:
-        audio = audio_queue.get()  # retrieve the next audio processing job from the main thread
+        audiosentence = audio_queue.get()  # retrieve the next audio processing job from the main thread
         if audio is None: break  # stop processing if the main thread is done
 
         # received audio data, now we'll recognize it using Google Speech Recognition
@@ -48,9 +45,8 @@ def recognize_worker():
             # for testing purposes, we're just using the default API key
             # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
             # instead of `r.recognize_google(audio)`
-            sentence = r.recognize_google(audio, language="tr-tr")
 
-            print("Google Speech Recognition thinks you said " + r.recognize_google(audio, language="tr-tr"))
+            print("Google Speech Recognition thinks you said " + record.recognize_google(audiosentence, language="tr-tr"))
         except sr.UnknownValueError:
             print("Google Speech Recognition could not understand audio")
         except sr.RequestError as e:
@@ -66,37 +62,67 @@ recognize_thread.start()
 with sr.Microphone() as source:
     try:
         while True:  # repeatedly listen for phrases and put the resulting audio on the audio processing job queue
-            audio = r.listen(source, timeout=5, phrase_time_limit=5)  # It takes the microphones audio data
+            audio = record.listen(source, timeout=5, phrase_time_limit=10)  # It takes the microphones audio data
             with open("output-example1.flac", "wb") as f:
                 f.write(audio.get_flac_data())
             # this segment will be taking the audio data and process through diarization and noise reduction
             # --------------------------------------------------------------------------------------
             # read audio data from file
             data, sample_rate = sf.read("output-example1.flac")
-            reduce_noise = nr.reduce_noise(y=data, sr=sample_rate)
+            reduce_noise = nr.reduce_noise(y=data, sr=sample_rate)  # apply the noise reduction
 
             # --------------------------------------------------------------------------------------
 
             sf.write("output-example4.wav", reduce_noise, samplerate=sample_rate)
             sf.write("output-example1.flac", reduce_noise, samplerate=sample_rate)
-            #try:
+            # try:
             #    diarization = pipeline("output-example4.wav")
-            #except ValueError:
+            # except ValueError:
             #    pass
+            with sr.AudioFile("output-example4.wav") as source_from_file:
+                audio = record.record(source_from_file)
 
-            source.audio = audio
-            #for turn, _, speaker in diarization.itertracks(yield_label=True):
+            # for turn, _, speaker in diarization.itertracks(yield_label=True):
             #    print(f"start={turn.start:.1f}s stop={turn.end:.1f}s speaker_{speaker}")
             try:
                 output = pipeline("output-example4.wav")
             except ValueError:
                 pass
-
+            combine = 0
             for turn, _, speaker in output.itertracks(yield_label=True):
+                # times between which to extract the wave from
+                start = turn.start  # seconds
+                end = turn.end  # seconds
+
+                if speaker[9] == "0":
+                    # file to extract the snippet from
+                    with wave.open('output-example4.wav', "rb") as infile:
+                        # get file data
+                        nchannels = infile.getnchannels()
+                        sampwidth = infile.getsampwidth()
+                        framerate = infile.getframerate()
+                        # set position in wave to start of segment
+                        infile.setpos(int(start * framerate))
+                        # extract data
+                        data = infile.readframes(int((end - start) * framerate))
+
+                    # write the extracted data to a new file
+                    with wave.open('outputfile.wav', 'w') as outfile:
+                        outfile.setnchannels(nchannels)
+                        outfile.setsampwidth(sampwidth)
+                        outfile.setframerate(framerate)
+                        outfile.setnframes(int(len(data) / sampwidth))
+                        outfile.writeframes(data)
+                    infile.close()
+                    outfile.close()
+
+                    the_result_audio_file = AudioSegment.from_wav("outputfile.wav")
+
+                    combine = combine + the_result_audio_file
+                    combine.export("C:/Users/serha/PycharmProjects/pythonProject/combined.wav", format='wav')
                 print(f"start={turn.start:.1f}s stop={turn.end:.1f}s speaker_{speaker}")
-
-            print(output.itertracks(yield_label=True))
-
+            with sr.AudioFile("combined.wav") as the_combined_data:
+                audio = record.record(the_combined_data)
             audio_queue.put(audio)
     except KeyboardInterrupt:  # allow Ctrl + C to shut down the program
         pass
@@ -104,3 +130,24 @@ with sr.Microphone() as source:
 audio_queue.join()  # block until all current audio processing jobs are done
 audio_queue.put(None)  # tell the recognize_thread to stop
 recognize_thread.join()  # wait for the recognize_thread to actually stop
+
+
+# a function that splits the audio file into chunks
+# and applies speech recognition
+def get_large_audio_transcription(path):
+    whole_text = ''
+    # open the audio file using pydub
+    sound = AudioSegment.from_wav(path)
+    # recognize the chunk
+    with sr.AudioFile(path) as source:
+        audio_listened = record.record(source)
+        # try converting it to text
+        try:
+            text = record.recognize_google(audio_listened)
+        except sr.UnknownValueError as e:
+            print("Error:", str(e))
+        else:
+            text = f"{text.capitalize()}. "
+            whole_text += text
+    # return the text for all chunks detected
+    return whole_text
